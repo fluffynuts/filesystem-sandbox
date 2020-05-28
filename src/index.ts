33c6f24e785eb00f909962cf95b6dd3e400a6793
Davyd McColl <davydm@gmail.com>
2020-05-28 09:02:28 +0200

    :tada: ready for consumption

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { promises as fs } from "fs";
+import { BaseEncodingOptions, promises as fs, Stats, StatsBase } from "fs";
+
 if (!fs) {
-  throw new Error("Yer node is olde! filesystem-sandboxes requires a Node with fs.promises");
+    throw new Error("Yer node is olde! filesystem-sandboxes requires a Node with fs.promises");
 }
 
 import { v4 as uuid } from "uuid";
@@ -8,62 +9,100 @@ import * as path from "path";
 import { sync as mkdir } from "mkdirp";
 import { sync as _rimraf } from "rimraf";
 
-const { writeFile, readFile } = fs;
+const { writeFile, readFile, stat } = fs;
 
-function rimraf(p: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-      try {
-        _rimraf(p);
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-  });
+async function isFolder(p: string): Promise<boolean> {
+    try {
+        const st = await stat(p);
+        return st && st.isDirectory();
+    } catch (e) {
+        return false;
+    }
+}
+
+async function rimraf(p: string): Promise<void> {
+    if (!(await isFolder(p))) {
+        return;
+    }
+    return new Promise((resolve, reject) => {
+        try {
+            _rimraf(p, { maxBusyTries: 5 });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
 }
 
 export const basePrefix = "__sandboxes__";
+
+const sandboxes: Sandbox[] = [];
+
 export class Sandbox {
-  private readonly _path: string;
+    private readonly _path: string;
+    private readonly _base: string;
 
-  get path() {
-    return this._path;
-  }
+    get path() {
+        return this._path;
+    }
 
-  constructor(at?: string) {
-    this._path = path.join((at || process.cwd()), basePrefix, uuid());
-    mkdir(this._path);
-  }
+    constructor(at?: string) {
+        this._base = path.join(at || process.cwd(), basePrefix);
+        this._path = path.join(this._base, uuid());
+        mkdir(this._path);
+        sandboxes.push(this);
+    }
 
-  async destroy(dir: string) {
-      await rimraf(dir);
-  }
+    public async destroy() {
+        const myIndex = sandboxes.indexOf(this);
+        if (myIndex > -1) {
+            sandboxes.splice(myIndex, 1);
+        }
+        await rimraf(this._path);
+        const contents = await fs.readdir(this._base);
+        if (!contents.length) {
+            await rimraf(this._base);
+        }
+    }
 
-  async writeTextFile(at: string, contents: string) {
-    const fullPath = this.fullPathFor(at);
-    await writeFile(
-      fullPath,
-      contents, { encoding: "utf8" }
-    );
-    return fullPath;
-  }
+    async writeFile(at: string, contents: string | Buffer) {
+        const
+            fullPath = this.fullPathFor(at),
+            options = contents instanceof Buffer
+                ? undefined
+                : { encoding: "utf8" } as BaseEncodingOptions;
+        await writeFile(
+            fullPath,
+            contents,
+            options
+        );
+        return fullPath;
+    }
 
-  async readTextFile(at: string) {
-    return readFile(
-      this.fullPathFor(at),
-      { encoding: "utf8" }
-    );
-  }
+    async readTextFile(at: string): Promise<string> {
+        return readFile(
+            this.fullPathFor(at),
+            { encoding: "utf8" }
+        );
+    }
 
-  fullPathFor(relativePath: string) {
-    return path.join(this._path, relativePath);
-  }
+    async readFile(at: string): Promise<Buffer> {
+        return readFile(this.fullPathFor(at));
+    }
 
-  public static async destroyAll() {
-    await rimraf(path.join(basePrefix));
-  }
+    fullPathFor(relativePath: string) {
+        return path.join(this._path, relativePath);
+    }
 
-  public static async create(at: string) {
-    return new Sandbox(at);
-  }
+    public static async destroyAll() {
+        const toDestroy = sandboxes.splice(0, sandboxes.length);
+        for (let sandbox of toDestroy) {
+            await sandbox.destroy();
+        }
+    }
+
+    public static async create(at: string) {
+        return new Sandbox(at);
+    }
 }
 
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,9 @@
 import "./test-helpers";
+import { promises as fs } from "fs";
 import * as path from "path";
-import { Sandbox, basePrefix } from "../src";
+import { basePrefix, Sandbox } from "../src";
+import * as faker from "faker";
+import rimraf from "rimraf";
 
 describe(`filesystem-sandbox`, () => {
     describe(`construction`, () => {
@@ -21,6 +24,164 @@ describe(`filesystem-sandbox`, () => {
                     .toBeFolder();
             });
         });
+        describe(`when given at`, () => {
+            it(`should create a sandbox dir under the given folder`, async () => {
+                // Arrange
+                const customBase = path.join(__dirname, faker.random.alphaNumeric(10));
+                // Act
+                const sut = create(customBase);
+                // Assert
+                expect(sut.path)
+                    .toStartWith(customBase);
+                expect(customBase)
+                    .toBeFolder();
+            });
+        });
+        describe(`destroy`, () => {
+            it(`should destroy the sandbox entirely`, async () => {
+                // Arrange
+                const sut = create();
+                expect(sut.path)
+                    .toBeFolder();
+                // Act
+                await sut.destroy();
+                // Assert
+                expect(sut.path)
+                    .not.toBeFolder();
+            });
+            it(`should remove the __sandboxes__ folder when none left`, async () => {
+                // Arrange
+                const
+                    sut1 = create(),
+                    sut2 = create();
+                // Act
+                await sut1.destroy();
+                expect(sut1.path)
+                    .not.toBeFolder();
+                expect(sut2.path)
+                    .toBeFolder();
+                expect(path.dirname(sut1.path))
+                    .toBeFolder();
+                await sut2.destroy();
+                // Assert
+                expect(sut2.path)
+                    .not.toBeFolder();
+                expect(path.dirname(sut2.path))
+                    .not.toBeFolder();
+            });
+        });
+
+        describe(`destroyAll`, () => {
+            it(`should destroy all sandboxes`, async () => {
+                // Arrange
+                const
+                    sut1 = create(),
+                    sut2 = create();
+                expect(sut1.path)
+                    .toBeFolder();
+                expect(sut2.path)
+                    .toBeFolder();
+                // Act
+                await Sandbox.destroyAll();
+                // Assert
+                expect(sut1.path)
+                    .not.toBeFolder()
+                expect(sut2.path)
+                    .not.toBeFolder();
+                expect(path.dirname(sut1.path))
+                    .not.toBeFolder();
+            });
+        });
+
+        describe(`file io`, () => {
+            it(`should be able to write a text file`, async () => {
+                // Arrange
+                const
+                    data = faker.random.words(50),
+                    filename = faker.random.alphaNumeric(10),
+                    sut = create();
+                // Act
+                const fullpath = await sut.writeFile(filename, data);
+                // Assert
+                expect(fullpath)
+                    .toBeFile();
+                const writtenData = await fs.readFile(fullpath, { encoding: "utf8" });
+                expect(writtenData)
+                    .toEqual(data);
+            });
+
+            it(`should be able to read a text file by relative path`, async () => {
+                // Arrange
+                const
+                    data = faker.random.words(50),
+                    filename = faker.random.alphaNumeric(10),
+                    sut = create();
+                // Act
+                const
+                    fullpath = await sut.writeFile(filename, data),
+                    written = await fs.readFile(fullpath, { encoding: "utf8" }),
+                    updated = written + faker.random.words(10);
+                await fs.writeFile(fullpath, updated, { encoding: "utf8" });
+                const result = await sut.readTextFile(filename);
+                // Assert
+                expect(result)
+                    .toEqual(updated);
+            });
+
+            it(`should be able to write a binary file`, async () => {
+                // Arrange
+                const
+                    data = randomBytes(),
+                    filename = faker.random.alphaNumeric(10),
+                    buffer = Buffer.from(data),
+                    sut = create();
+                // Act
+                const
+                    fullPath = await sut.writeFile(filename, buffer),
+                    result = await fs.readFile(fullPath);
+                // Assert
+                expect(result)
+                    .toEqual(buffer);
+            });
+
+            it(`should be able to read a binary file`, async () => {
+                // Arrange
+                const
+                    filename = faker.random.alphaNumeric(10),
+                    buffer = Buffer.from(randomBytes()),
+                    newData = Buffer.from(randomBytes()),
+                    sut = create();
+                // Act
+                const
+                    fullPath = await sut.writeFile(filename, buffer),
+                    written = await fs.readFile(fullPath),
+                    updated = Buffer.concat([written, newData]);
+                await fs.writeFile(fullPath, updated);
+                const result = await sut.readFile(filename);
+                // Assert
+                expect(result)
+                    .toEqual(updated);
+            });
+
+            function randomBytes() {
+                const
+                    length = faker.random.number({ min: 500, max: 1024 }),
+                    result = new Array(length);
+
+                for (let i = 0; i < length; i++) {
+                    result[i] = faker.random.number({ min: 0, max: 255});
+                }
+                return result;
+            }
+        });
+    });
+
+    beforeAll(() => {
+        // in case there's something hanging about from a prior test run
+        rimraf.sync(basePrefix);
+    });
+    afterEach(async () => {
+        await Sandbox.destroyAll();
     });
 
     function create(at?: string) {
